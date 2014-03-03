577f2d8d35fedcf576585dea5ed96d330f580289
Martin Schimak <martin.schimak@plexiti.com>
2014-03-03 17:32:43 +0100

    Reduce test code in order to speed up test time. Different name configurations will be unit tested.

--- test/cli/CreateProcessTests.groovy
+++ test/cli/CreateProcessTests.groovy
@@ -3,17 +3,6 @@ import grails.test.AbstractCliTestCase
 class CreateProcessTests extends AbstractCliTestCase {
 
     void testCreateProcess() {
-       [
-           "com.plexiti.Sample",
-           "com.plexiti.Sample.bpmn",
-           "com.plexiti.SampleProcess",
-           "com.plexiti.SampleProcess.bpmn",
-       ].each { processName ->
-           createProcess(processName)
-       }
-    }
-    
-    void createProcess(String processName) {
 
         def bpmnFile = new File("${workDir}/grails-app/processes/com/plexiti/SampleProcess.bpmn")
         def testFile = new File("${workDir}/test/integration/com/plexiti/SampleProcessSpec.groovy")
@@ -21,7 +10,7 @@ class CreateProcessTests extends AbstractCliTestCase {
         assert !testFile.exists()
 
         try {
-            execute(["create-process", processName])
+            execute(["create-process", "com.plexiti.SampleProcess"])
             assert waitForProcess() == 0
             verifyHeader()
             assert bpmnFile.exists()
