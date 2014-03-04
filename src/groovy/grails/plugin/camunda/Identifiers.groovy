b99e707deadeda208e26303534b257a6b14fab4f
Martin Schimak <martin.schimak@plexiti.com>
2014-03-04 08:57:15 +0100

    improve(script): take care of default package
    Issue #7

--- src/groovy/grails/plugin/camunda/Identifiers.groovy
+++ src/groovy/grails/plugin/camunda/Identifiers.groovy
@@ -9,9 +9,13 @@ class Identifiers {
         def identifier = '[a-zA-Z_][a-zA-Z0-9_]*'
         def name = fullName - ~"\\.${Constants.EXTENSION}\$"
         assert name =~ "^(${identifier}\\.)*(${identifier})\$" : "The package and name of your " +
-                "new process definition (name = $name) does not qualify as a valid Java identifier. For " +
-                "compatibility reasons, please choose a name which would qualify as a valid Java class name."
-        [name.substring(0, name.lastIndexOf('.')), name.substring(name.lastIndexOf('.') + 1) - ~"$Constants.TYPE\$"]
+            "new process definition (name = $name) does not qualify as a valid Java identifier. For " +
+            "compatibility reasons, please choose a name which qualifies as a valid Java class name " +
+            "(including package)."
+        [
+            name.substring(0, name.contains('.') ? name.lastIndexOf('.') : 0), 
+            name.substring(name.lastIndexOf('.') + 1) - ~"$Constants.TYPE\$"
+        ]
     }
 
 }
