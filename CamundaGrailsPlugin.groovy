11cb6381ba334163eaa8072cad3a091d8734d194
Martin Schimak <martin.schimak@plexiti.com>
2014-03-07 13:28:20 +0100

    test(engine): add default test process definition and integration test

--- CamundaGrailsPlugin.groovy
+++ CamundaGrailsPlugin.groovy
@@ -1,25 +1,26 @@
 import grails.plugin.camunda.CamundaPluginSupport
+import grails.plugin.camunda.Constants
 
 class CamundaGrailsPlugin {
 
     def title = "camunda BPM Plugin"
+    def version = "0.1.0-SNAPSHOT"
     def author = "Martin Schimak"
-    def organization = [ name: "plexiti GmbH", url: "http://plexiti.com" ]
     def authorEmail = "martin.schimak@plexiti.com"
+    def organization = [ name: "plexiti GmbH", url: "http://plexiti.com" ]
+    def license = "APACHE"
 
     def description = '''\
 This plugin integrates the camunda BPM platform with Grails. camunda BPM is a flexible framework for 
 workflow and process automation. It's core is a native BPMN 2.0 process engine that runs inside the Java 
 Virtual Machine. It is a perfect match for the Spring Framework - and therefore for Grails, too.'''
 
-    def version = "0.1.0-SNAPSHOT"
     def documentation = "http://plexiti.github.io/camunda-grails-plugin"
     def scm = [ url: "https://github.com/plexiti/camunda-grails-plugin" ]
     def issueManagement = [ system: "github", url: "https://github.com/plexiti/camunda-grails-plugin/issues" ]
-    def license = "APACHE"
     def grailsVersion = "2.3 > *"
 
-    def pluginExcludes = [ "grails-app/views/error.gsp" ]
+    def pluginExcludes = [ "grails-app/views/error.gsp", "grails-app/processes/" ]
 
     def doWithSpring = CamundaPluginSupport.doWithSpring
 
