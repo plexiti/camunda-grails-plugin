5cfba179e973e0963981d32b7e255b9301db3ca4
Martin Schimak <martin.schimak@plexiti.com>
2014-03-07 21:59:53 +0100

    docs(project): first two chapters of documentation
    Issue #12

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,5 +1,17 @@
 // configuration for plugin testing - will not be included in the plugin zip
 
+grails.doc.title = "camunda Grails plugin"
+grails.doc.subtitle = """The camunda Grails plugin integrates the camunda BPM platform with the Grails web application framework."""
+grails.doc.copyright = """\
+<div>
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+<img alt="Creative Commons 4.0" style="border-width:0; vertical-align: middle" src="http://i.creativecommons.org/l/by/4.0/80x15.png"></img></a>
+<span style="vertical-align: middle"><a href="http://github.com/martinschimak" property="cc:attributionName" rel="cc:attributionURL">Martin Schimak</a>, 
+<a href="http://plexiti.com" property="cc:attributionName" rel="cc:attributionURL">plexiti GmbH</a></span>
+</div>
+"""
+grails.doc.images = new File("src/docs/images")
+
 log4j = {
     // Example of changing the log pattern for the default console
     // appender:
