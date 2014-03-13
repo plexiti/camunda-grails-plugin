3107056763a31a6f8aa5e19af8351406f8aac647
Martin Schimak <martin.schimak@plexiti.com>
2014-03-13 12:51:37 +0100

    release(project): prepare release 0.1.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -69,7 +69,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.0.0"
         compile ':cache:1.1.1'
-        compile ':camunda:0.1.0-SNAPSHOT'
+        compile ':camunda:0.1.0'
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.1" // or ":hibernate4:4.1.11.1"
