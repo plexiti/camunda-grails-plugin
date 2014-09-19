d235b1b6ad9ebff9e28acb9f4e66d85687bc20af
Martin Schimak <martin.schimak@plexiti.com>
2014-09-19 22:52:42 +0200

    chore(util): add code to enable check whether a given camunda BPM version or higher version is used or not used

---
+++ src/groovy/grails/plugin/camunda/CamundaBpmVersion.groovy
@@ -0,0 +1,63 @@
+package grails.plugin.camunda
+
+import org.camunda.bpm.engine.ProcessEngine
+
+import java.util.regex.Pattern
+
+/**
+ * @author Martin Schimak <martin.schimak@plexiti.com>
+ */
+class CamundaBpmVersion {
+    
+    protected static String testVersion
+    protected static Pattern versionPattern = ~/^(\d+)\.(\d+)\.(\d+)-(alpha|beta|Final)(\d*)$/
+    
+    public static boolean isAtLeast(String neededVersion) {
+        def neededMatcher = versionPattern.matcher(neededVersion)
+        if (!neededMatcher.matches()) {
+            throw new IllegalArgumentException("Needed version '$neededVersion' does not match expected pattern $versionPattern.")
+        }
+        def actualVersion = getVersion()
+        def actualMatcher = versionPattern.matcher(actualVersion)
+        if (!actualMatcher.matches()) {
+            throw new UnknownFormatConversionException("camunda BPM version '$actualVersion' does not match expected pattern $versionPattern.")
+        }
+        def actual = actualMatcher[0][1] as int
+        def needed = neededMatcher[0][1] as int
+        if (actual > needed) {
+            return true
+        } else if (actual == needed) {
+            actual = actualMatcher[0][2] as int
+            needed = neededMatcher[0][2] as int
+            if (actual > needed) {
+                return true
+            } else if (actual == needed) {
+                actual = actualMatcher[0][3] as int
+                needed = neededMatcher[0][3] as int
+                if (actual > needed) {
+                    return true
+                } else if (actual == needed) {
+                    actual = actualMatcher[0][4] as String
+                    needed = neededMatcher[0][4] as String
+                    if (actual.toLowerCase().compareTo(needed.toLowerCase()) > 0) {
+                        return true
+                    } else if (actual == needed) {
+                        actual = (actualMatcher[0][5] ?: 0) as int
+                        needed = (neededMatcher[0][5] ?: 0) as int
+                        if (actual >= needed) {
+                            return true
+                        }
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    public static String getVersion() {
+        if (testVersion)
+            return testVersion
+        ProcessEngine.class.package.implementationVersion 
+    }
+
+}
---
+++ test/unit/grails/plugin/camunda/CamundaBpmVersionSpec.groovy
@@ -0,0 +1,278 @@
+package grails.plugin.camunda
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+/**
+ * @author Martin Schimak <martin.schimak@plexiti.com>
+ */
+class CamundaBpmVersionSpec extends Specification {
+
+    void "Test whether camunda BPM version is retrievable"() {
+
+        when: "we retrieve the actual camunda BPM version"
+        def version = CamundaBpmVersion.version
+
+        then: "we expect to see a specific pattern"
+        version == "7.1.0-Final"
+        
+    }
+    
+    @Unroll
+    void "Test whether camunda BPM #actualVersion is at least #neededVersion"() {
+
+        given: "we set a camunda BPM version as test value"
+        CamundaBpmVersion.testVersion = actualVersion
+        
+        when: "we check for a specific needed version"
+        def actualResult = CamundaBpmVersion.isAtLeast(neededVersion)
+        
+        then: "the needed version is greater than the actual version - or not"
+        actualResult == expectedResult
+        
+        where:
+        actualVersion  | neededVersion  | expectedResult
+        "7.0.0-alpha1" | "7.0.0-alpha1" | true
+        "7.0.0-alpha2" | "7.0.0-alpha1" | true
+        "7.0.0-beta1"  | "7.0.0-alpha1" | true
+        "7.0.0-beta2"  | "7.0.0-alpha1" | true
+        "7.0.0-Final"  | "7.0.0-alpha1" | true
+        "7.1.0-alpha1" | "7.0.0-alpha1" | true
+        "7.1.0-alpha2" | "7.0.0-alpha1" | true
+        "7.1.0-beta1"  | "7.0.0-alpha1" | true
+        "7.1.0-beta2"  | "7.0.0-alpha1" | true
+        "7.1.0-Final"  | "7.0.0-alpha1" | true
+        "7.2.0-alpha1" | "7.0.0-alpha1" | true
+        "7.2.0-alpha2" | "7.0.0-alpha1" | true
+        "7.2.0-beta1"  | "7.0.0-alpha1" | true
+        "7.2.0-beta2"  | "7.0.0-alpha1" | true
+        "7.2.0-Final"  | "7.0.0-alpha1" | true
+
+        "7.0.0-alpha1" | "7.0.0-alpha2" | false
+        "7.0.0-alpha2" | "7.0.0-alpha2" | true
+        "7.0.0-beta1"  | "7.0.0-alpha2" | true
+        "7.0.0-beta2"  | "7.0.0-alpha2" | true
+        "7.0.0-Final"  | "7.0.0-alpha2" | true
+        "7.1.0-alpha1" | "7.0.0-alpha2" | true
+        "7.1.0-alpha2" | "7.0.0-alpha2" | true
+        "7.1.0-beta1"  | "7.0.0-alpha2" | true
+        "7.1.0-beta2"  | "7.0.0-alpha2" | true
+        "7.1.0-Final"  | "7.0.0-alpha2" | true
+        "7.2.0-alpha1" | "7.0.0-alpha2" | true
+        "7.2.0-alpha2" | "7.0.0-alpha2" | true
+        "7.2.0-beta1"  | "7.0.0-alpha2" | true
+        "7.2.0-beta2"  | "7.0.0-alpha2" | true
+        "7.2.0-Final"  | "7.0.0-alpha2" | true
+
+        "7.0.0-alpha1" | "7.0.0-beta1" | false
+        "7.0.0-alpha2" | "7.0.0-beta1" | false
+        "7.0.0-beta1"  | "7.0.0-beta1" | true
+        "7.0.0-beta2"  | "7.0.0-beta1" | true
+        "7.0.0-Final"  | "7.0.0-beta1" | true
+        "7.1.0-alpha1" | "7.0.0-beta1" | true
+        "7.1.0-alpha2" | "7.0.0-beta1" | true
+        "7.1.0-beta1"  | "7.0.0-beta1" | true
+        "7.1.0-beta2"  | "7.0.0-beta1" | true
+        "7.1.0-Final"  | "7.0.0-beta1" | true
+        "7.2.0-alpha1" | "7.0.0-beta1" | true
+        "7.2.0-alpha2" | "7.0.0-beta1" | true
+        "7.2.0-beta1"  | "7.0.0-beta1" | true
+        "7.2.0-beta2"  | "7.0.0-beta1" | true
+        "7.2.0-Final"  | "7.0.0-beta1" | true
+
+        "7.0.0-alpha1" | "7.0.0-beta2" | false
+        "7.0.0-alpha2" | "7.0.0-beta2" | false
+        "7.0.0-beta1"  | "7.0.0-beta2" | false
+        "7.0.0-beta2"  | "7.0.0-beta2" | true
+        "7.0.0-Final"  | "7.0.0-beta2" | true
+        "7.1.0-alpha1" | "7.0.0-beta2" | true
+        "7.1.0-alpha2" | "7.0.0-beta2" | true
+        "7.1.0-beta1"  | "7.0.0-beta2" | true
+        "7.1.0-beta2"  | "7.0.0-beta2" | true
+        "7.1.0-Final"  | "7.0.0-beta2" | true
+        "7.2.0-alpha1" | "7.0.0-beta2" | true
+        "7.2.0-alpha2" | "7.0.0-beta2" | true
+        "7.2.0-beta1"  | "7.0.0-beta2" | true
+        "7.2.0-beta2"  | "7.0.0-beta2" | true
+        "7.2.0-Final"  | "7.0.0-beta2" | true
+
+        "7.0.0-alpha1" | "7.0.0-Final" | false
+        "7.0.0-alpha2" | "7.0.0-Final" | false
+        "7.0.0-beta1"  | "7.0.0-Final" | false
+        "7.0.0-beta2"  | "7.0.0-Final" | false
+        "7.0.0-Final"  | "7.0.0-Final" | true
+        "7.1.0-alpha1" | "7.0.0-Final" | true
+        "7.1.0-alpha2" | "7.0.0-Final" | true
+        "7.1.0-beta1"  | "7.0.0-Final" | true
+        "7.1.0-beta2"  | "7.0.0-Final" | true
+        "7.1.0-Final"  | "7.0.0-Final" | true
+        "7.2.0-alpha1" | "7.0.0-Final" | true
+        "7.2.0-alpha2" | "7.0.0-Final" | true
+        "7.2.0-beta1"  | "7.0.0-Final" | true
+        "7.2.0-beta2"  | "7.0.0-Final" | true
+        "7.2.0-Final"  | "7.0.0-Final" | true
+
+        "7.0.0-alpha1" | "7.1.0-alpha1" | false
+        "7.0.0-alpha2" | "7.1.0-alpha1" | false
+        "7.0.0-beta1"  | "7.1.0-alpha1" | false
+        "7.0.0-beta2"  | "7.1.0-alpha1" | false
+        "7.0.0-Final"  | "7.1.0-alpha1" | false
+        "7.1.0-alpha1" | "7.1.0-alpha1" | true
+        "7.1.0-alpha2" | "7.1.0-alpha1" | true
+        "7.1.0-beta1"  | "7.1.0-alpha1" | true
+        "7.1.0-beta2"  | "7.1.0-alpha1" | true
+        "7.1.0-Final"  | "7.1.0-alpha1" | true
+        "7.2.0-alpha1" | "7.1.0-alpha1" | true
+        "7.2.0-alpha2" | "7.1.0-alpha1" | true
+        "7.2.0-beta1"  | "7.1.0-alpha1" | true
+        "7.2.0-beta2"  | "7.1.0-alpha1" | true
+        "7.2.0-Final"  | "7.1.0-alpha1" | true
+
+        "7.0.0-alpha1" | "7.1.0-alpha2" | false
+        "7.0.0-alpha2" | "7.1.0-alpha2" | false
+        "7.0.0-beta1"  | "7.1.0-alpha2" | false
+        "7.0.0-beta2"  | "7.1.0-alpha2" | false
+        "7.0.0-Final"  | "7.1.0-alpha2" | false
+        "7.1.0-alpha1" | "7.1.0-alpha2" | false
+        "7.1.0-alpha2" | "7.1.0-alpha2" | true
+        "7.1.0-beta1"  | "7.1.0-alpha2" | true
+        "7.1.0-beta2"  | "7.1.0-alpha2" | true
+        "7.1.0-Final"  | "7.1.0-alpha2" | true
+        "7.2.0-alpha1" | "7.1.0-alpha2" | true
+        "7.2.0-alpha2" | "7.1.0-alpha2" | true
+        "7.2.0-beta1"  | "7.1.0-alpha2" | true
+        "7.2.0-beta2"  | "7.1.0-alpha2" | true
+        "7.2.0-Final"  | "7.1.0-alpha2" | true
+
+        "7.0.0-alpha1" | "7.1.0-beta1" | false
+        "7.0.0-alpha2" | "7.1.0-beta1" | false
+        "7.0.0-beta1"  | "7.1.0-beta1" | false
+        "7.0.0-beta2"  | "7.1.0-beta1" | false
+        "7.0.0-Final"  | "7.1.0-beta1" | false
+        "7.1.0-alpha1" | "7.1.0-beta1" | false
+        "7.1.0-alpha2" | "7.1.0-beta1" | false
+        "7.1.0-beta1"  | "7.1.0-beta1" | true
+        "7.1.0-beta2"  | "7.1.0-beta1" | true
+        "7.1.0-Final"  | "7.1.0-beta1" | true
+        "7.2.0-alpha1" | "7.1.0-beta1" | true
+        "7.2.0-alpha2" | "7.1.0-beta1" | true
+        "7.2.0-beta1"  | "7.1.0-beta1" | true
+        "7.2.0-beta2"  | "7.1.0-beta1" | true
+        "7.2.0-Final"  | "7.1.0-beta1" | true
+
+        "7.0.0-alpha1" | "7.1.0-beta2" | false
+        "7.0.0-alpha2" | "7.1.0-beta2" | false
+        "7.0.0-beta1"  | "7.1.0-beta2" | false
+        "7.0.0-beta2"  | "7.1.0-beta2" | false
+        "7.0.0-Final"  | "7.1.0-beta2" | false
+        "7.1.0-alpha1" | "7.1.0-beta2" | false
+        "7.1.0-alpha2" | "7.1.0-beta2" | false
+        "7.1.0-beta1"  | "7.1.0-beta2" | false
+        "7.1.0-beta2"  | "7.1.0-beta2" | true
+        "7.1.0-Final"  | "7.1.0-beta2" | true
+        "7.2.0-alpha1" | "7.1.0-beta2" | true
+        "7.2.0-alpha2" | "7.1.0-beta2" | true
+        "7.2.0-beta1"  | "7.1.0-beta2" | true
+        "7.2.0-beta2"  | "7.1.0-beta2" | true
+        "7.2.0-Final"  | "7.1.0-beta2" | true
+
+        "7.0.0-alpha1" | "7.1.0-Final" | false
+        "7.0.0-alpha2" | "7.1.0-Final" | false
+        "7.0.0-beta1"  | "7.1.0-Final" | false
+        "7.0.0-beta2"  | "7.1.0-Final" | false
+        "7.0.0-Final"  | "7.1.0-Final" | false
+        "7.1.0-alpha1" | "7.1.0-Final" | false
+        "7.1.0-alpha2" | "7.1.0-Final" | false
+        "7.1.0-beta1"  | "7.1.0-Final" | false
+        "7.1.0-beta2"  | "7.1.0-Final" | false
+        "7.1.0-Final"  | "7.1.0-Final" | true
+        "7.2.0-alpha1" | "7.1.0-Final" | true
+        "7.2.0-alpha2" | "7.1.0-Final" | true
+        "7.2.0-beta1"  | "7.1.0-Final" | true
+        "7.2.0-beta2"  | "7.1.0-Final" | true
+        "7.2.0-Final"  | "7.1.0-Final" | true
+
+        "7.0.0-alpha1" | "7.2.0-alpha1" | false
+        "7.0.0-alpha2" | "7.2.0-alpha1" | false
+        "7.0.0-beta1"  | "7.2.0-alpha1" | false
+        "7.0.0-beta2"  | "7.2.0-alpha1" | false
+        "7.0.0-Final"  | "7.2.0-alpha1" | false
+        "7.1.0-alpha1" | "7.2.0-alpha1" | false
+        "7.1.0-alpha2" | "7.2.0-alpha1" | false
+        "7.1.0-beta1"  | "7.2.0-alpha1" | false
+        "7.1.0-beta2"  | "7.2.0-alpha1" | false
+        "7.1.0-Final"  | "7.2.0-alpha1" | false
+        "7.2.0-alpha1" | "7.2.0-alpha1" | true
+        "7.2.0-alpha2" | "7.2.0-alpha1" | true
+        "7.2.0-beta1"  | "7.2.0-alpha1" | true
+        "7.2.0-beta2"  | "7.2.0-alpha1" | true
+        "7.2.0-Final"  | "7.2.0-alpha1" | true
+
+        "7.0.0-alpha1" | "7.2.0-alpha2" | false
+        "7.0.0-alpha2" | "7.2.0-alpha2" | false
+        "7.0.0-beta1"  | "7.2.0-alpha2" | false
+        "7.0.0-beta2"  | "7.2.0-alpha2" | false
+        "7.0.0-Final"  | "7.2.0-alpha2" | false
+        "7.1.0-alpha1" | "7.2.0-alpha2" | false
+        "7.1.0-alpha2" | "7.2.0-alpha2" | false
+        "7.1.0-beta1"  | "7.2.0-alpha2" | false
+        "7.1.0-beta2"  | "7.2.0-alpha2" | false
+        "7.1.0-Final"  | "7.2.0-alpha2" | false
+        "7.2.0-alpha1" | "7.2.0-alpha2" | false
+        "7.2.0-alpha2" | "7.2.0-alpha2" | true
+        "7.2.0-beta1"  | "7.2.0-alpha2" | true
+        "7.2.0-beta2"  | "7.2.0-alpha2" | true
+        "7.2.0-Final"  | "7.2.0-alpha2" | true
+
+        "7.0.0-alpha1" | "7.2.0-beta1" | false
+        "7.0.0-alpha2" | "7.2.0-beta1" | false
+        "7.0.0-beta1"  | "7.2.0-beta1" | false
+        "7.0.0-beta2"  | "7.2.0-beta1" | false
+        "7.0.0-Final"  | "7.2.0-beta1" | false
+        "7.1.0-alpha1" | "7.2.0-beta1" | false
+        "7.1.0-alpha2" | "7.2.0-beta1" | false
+        "7.1.0-beta1"  | "7.2.0-beta1" | false
+        "7.1.0-beta2"  | "7.2.0-beta1" | false
+        "7.1.0-Final"  | "7.2.0-beta1" | false
+        "7.2.0-alpha1" | "7.2.0-beta1" | false
+        "7.2.0-alpha2" | "7.2.0-beta1" | false
+        "7.2.0-beta1"  | "7.2.0-beta1" | true
+        "7.2.0-beta2"  | "7.2.0-beta1" | true
+        "7.2.0-Final"  | "7.2.0-beta1" | true
+
+        "7.0.0-alpha1" | "7.2.0-beta2" | false
+        "7.0.0-alpha2" | "7.2.0-beta2" | false
+        "7.0.0-beta1"  | "7.2.0-beta2" | false
+        "7.0.0-beta2"  | "7.2.0-beta2" | false
+        "7.0.0-Final"  | "7.2.0-beta2" | false
+        "7.1.0-alpha1" | "7.2.0-beta2" | false
+        "7.1.0-alpha2" | "7.2.0-beta2" | false
+        "7.1.0-beta1"  | "7.2.0-beta2" | false
+        "7.1.0-beta2"  | "7.2.0-beta2" | false
+        "7.1.0-Final"  | "7.2.0-beta2" | false
+        "7.2.0-alpha1" | "7.2.0-beta2" | false
+        "7.2.0-alpha2" | "7.2.0-beta2" | false
+        "7.2.0-beta1"  | "7.2.0-beta2" | false
+        "7.2.0-beta2"  | "7.2.0-beta2" | true
+        "7.2.0-Final"  | "7.2.0-beta2" | true
+
+        "7.0.0-alpha1" | "7.2.0-Final" | false
+        "7.0.0-alpha2" | "7.2.0-Final" | false
+        "7.0.0-beta1"  | "7.2.0-Final" | false
+        "7.0.0-beta2"  | "7.2.0-Final" | false
+        "7.0.0-Final"  | "7.2.0-Final" | false
+        "7.1.0-alpha1" | "7.2.0-Final" | false
+        "7.1.0-alpha2" | "7.2.0-Final" | false
+        "7.1.0-beta1"  | "7.2.0-Final" | false
+        "7.1.0-beta2"  | "7.2.0-Final" | false
+        "7.1.0-Final"  | "7.2.0-Final" | false
+        "7.2.0-alpha1" | "7.2.0-Final" | false
+        "7.2.0-alpha2" | "7.2.0-Final" | false
+        "7.2.0-beta1"  | "7.2.0-Final" | false
+        "7.2.0-beta2"  | "7.2.0-Final" | false
+        "7.2.0-Final"  | "7.2.0-Final" | true
+        
+    }
+
+}
