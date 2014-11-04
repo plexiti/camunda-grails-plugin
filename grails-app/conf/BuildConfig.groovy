ae9df5ad613770c597844d497af921ab8e721594
Martin Schimak <martin.schimak@plexiti.com>
2014-11-04 14:25:10 +0100

    chore(configuration): upgrade dependency to camunda-grails-plugin-0.4.0-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -68,7 +68,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.0"
         compile ':cache:1.1.3'
         compile ':asset-pipeline:1.8.3'
-        compile ':camunda:0.3.0-SNAPSHOT'
+        compile ':camunda:0.4.0-SNAPSHOT'
         runtime ":hibernate:3.6.10.14" // or ":hibernate4:4.3.5.2"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.0.2"
