0be395b8baf9fa474675962ee4dfb9406b90b1fe
Martin Schimak <martin.schimak@plexiti.com>
2014-11-23 13:49:09 +0100

    test(war): test war file generation

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,6 +6,7 @@ grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.war.exploded = true
 
 grails.project.fork = [
     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
