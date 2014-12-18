9c8dec186470a6b737c727c8f666598245bbc97e
Martin Schimak <martin.schimak@plexiti.com>
2014-12-18 20:49:45 +0100

    test(beans): add dependency injection spec which respects older versions of camunda BPM than 7.2.0

---
+++ test/integration/grails/plugin/camunda/test/DependencyInjectionSpec.groovy
@@ -0,0 +1,43 @@
+package grails.plugin.camunda.test
+
+import grails.plugin.camunda.CamundaBpmVersion
+import spock.lang.Specification
+
+/**
+ * @author Martin Schimak <martin.schimak@plexiti.com>
+ */
+class DependencyInjectionSpec extends Specification {
+
+  def processEngine
+  def processEngineConfiguration
+  def processEngineService
+  def processApplication
+
+  def runtimeService
+  def repositoryService
+  def taskService
+  def managementService
+  def identityService
+  def authorizationService
+  def formService
+  def caseService
+  def filterService
+
+  void "Test successful dependency injection of standard beans"() {
+    expect:
+      processEngine
+      processEngineConfiguration
+      !processEngineService
+      !processApplication
+      repositoryService
+      taskService
+      managementService
+      identityService
+      authorizationService
+      runtimeService
+      formService
+      CamundaBpmVersion.isAtLeast('7.2.0') ? caseService : true
+      CamundaBpmVersion.isAtLeast('7.2.0') ? filterService : true
+  }
+
+}
