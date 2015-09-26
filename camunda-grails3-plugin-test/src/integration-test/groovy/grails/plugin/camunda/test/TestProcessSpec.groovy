a0a2d2b5f3fb6ae9c2620fa8f6a3bf2911b4add1
Martin Schimak <martin.schimak@plexiti.com>
2015-09-26 16:47:40 +0200

    feat(grails-3-support): add a first version of grails3-plugin tests

---
+++ camunda-grails3-plugin-test/src/integration-test/groovy/grails/plugin/camunda/test/TestProcessSpec.groovy
@@ -0,0 +1,64 @@
+package grails.plugin.camunda.test
+
+import org.camunda.bpm.engine.RuntimeService
+import grails.test.mixin.integration.Integration
+import grails.transaction.Rollback
+import grails.plugin.camunda.test.SampleService
+import org.camunda.bpm.engine.TaskService
+import org.camunda.bpm.engine.runtime.Execution
+import org.camunda.bpm.engine.test.mock.Mocks
+import spock.lang.Specification
+
+/**
+ * Integration Test for camunda TestProcess 
+ */
+@Integration
+@Rollback
+class TestProcessSpec extends Specification {
+
+    /**
+     * 1) Inject camunda process engine API service beans
+     */
+    RuntimeService runtimeService
+    TaskService taskService
+
+    /**
+     * 2) Mock your Grail(s) services called from TestProcess
+     */
+    def sampleTestProcessService = Mock(SampleService)
+
+    /**
+     * 3) Register your service mocks to make them accessible via TestProcess
+     */
+    def setup() {
+        Mocks.register("sampleService", sampleTestProcessService)
+    }
+
+    def cleanup() {
+        Mocks.reset()
+    }
+
+    /**
+     * 4) Test the various aspects and behaviour of TestProcess
+     */
+    void "Testing a happy walk through TestProcess"() {
+
+        given: "a new instance of TestProcess"
+        runtimeService.startProcessInstanceByKey("TestProcess")
+
+        when: "completing the user task"
+        def task = taskService.createTaskQuery().singleResult()
+        taskService.complete(task.id)
+
+        then: "the service method defined for the subsequent service task was called exactly once"
+        1 * sampleTestProcessService.serviceMethod(_ as Execution)
+
+        and: "nothing else was called"
+        0 * _
+
+        and: "the process instance finished"
+        !runtimeService.createProcessInstanceQuery().singleResult()
+
+    }
+
+}
