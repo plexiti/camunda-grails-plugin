b46fb75cde8d4a4e040ea33b9d06eb729c230c92
Martin Schimak <martin.schimak@plexiti.com>
2015-10-03 22:17:43 +0200

    refactor(grails3-support): move code from script to class and use constants

--- camunda-grails3-plugin/src/main/groovy/grails/plugin/camunda/Constants.groovy
+++ camunda-grails3-plugin/src/main/groovy/grails/plugin/camunda/Constants.groovy
@@ -24,4 +24,21 @@ public interface Constants {
     def EXTENSION = "bpmn";
     def PROCESS_PATH = "grails-app/processes";
 
+    /**
+     * Generate proper package and name for a given full name
+     * @param fullName the full name of the process definition
+     */
+    def generate = { String fullName ->
+        def identifier = '[a-zA-Z_][a-zA-Z0-9_]*'
+        def name = fullName - ~"\\.${EXTENSION}\$"
+        assert name =~ "^(${identifier}\\.)*(${identifier})\$" : "The package and name of your " +
+          "new process definition (name = $name) does not qualify as a valid Java identifier. For " +
+          "compatibility reasons, please choose a name which qualifies as a valid Java class name " +
+          "(including package)."
+        [
+          name.substring(0, name.contains('.') ? name.lastIndexOf('.') : 0),
+          name.substring(name.lastIndexOf('.') + 1) - ~"$TYPE\$"
+        ]
+    }
+    
 }
--- camunda-grails3-plugin/src/main/scripts/create-process.groovy
+++ camunda-grails3-plugin/src/main/scripts/create-process.groovy
@@ -14,37 +14,28 @@
  * limitations under the License.
  */
 
+import grails.plugin.camunda.Constants
+
 /**
  * Grails script that creates a Camunda BPM process definition
  *
  * @author Martin Schimak <martin.schimak@plexiti.com>
  * @since 0.1 (migrated to a Grails 3.x script with 0.6)
  */
-
 description ("Creates a new Camunda BPM process definition.") {
   usage "grails create-process [NAME]"
-  argument name:'Name', description:"The package and name of the process definition, e.g. my.package.SampleProcess"
-  flag name:'force', description:"Whether to overwrite existing files"
+  argument name:'Name', description: "The package and name of the process definition, e.g. my.package.SampleProcess"
+  flag name:'force', description: "Whether to overwrite existing files"
 }
 
-def generate = { String fullName, String type, String extension ->
-  def identifier = '[a-zA-Z_][a-zA-Z0-9_]*'
-  def name = fullName - ~"\\.${extension}\$"
-  assert name =~ "^(${identifier}\\.)*(${identifier})\$" : "The package and name of your " +
-    "new process definition (name = $name) does not qualify as a valid Java identifier. For " +
-    "compatibility reasons, please choose a name which qualifies as a valid Java class name " +
-    "(including package)."
-  [
-    name.substring(0, name.contains('.') ? name.lastIndexOf('.') : 0),
-    name.substring(name.lastIndexOf('.') + 1) - ~"$type\$"
-  ]
-}
+def classLoader = this.class.classLoader
+def constants = classLoader.loadClass("grails.plugin.camunda.Constants") as Constants
 
-def path = "grails-app/processes"
-def type = "Process"
-def extension = "bpmn"
+def path = constants.PROCESS_PATH
+def type = constants.TYPE
+def extension = constants.EXTENSION
 
-def (pkg, name) = generate(args[0], type, extension)
+def (pkg, name) = constants.generate(args[0])
 def artifact = "${pkg}.${name}${type}"
 def model = model(artifact)
 def overwrite = flag('force') ? true : false
